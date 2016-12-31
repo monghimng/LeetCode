af2b3aac3b26bf66a1332526c88d43a3cdc1a6b3
mong <monghim.ng@gmail.com>
2016-12-30 19:21:55 -0800

    init

---
+++ src/array/TwoSum.java
@@ -0,0 +1,16 @@
+package array;
+
+public class TwoSum {
+
+
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < nums.length; j++){
+                if(target - nums[i] - nums[j] == 0){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
